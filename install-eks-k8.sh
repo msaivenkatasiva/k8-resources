0f6942a8ea851f4129ef20438506156443a07c1b
msaivenkatasiva <sivasl0597@gmail.com>
2025-11-24 21:59:34 +0530

    kubernetes

---
+++ install-eks-k8.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/
