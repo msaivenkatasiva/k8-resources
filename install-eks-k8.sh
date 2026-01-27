2146771dd9978b3865b5e64238e33d1c0c8ef9e5
msaivenkatasiva <sivasl0597@gmail.com>
2026-01-27 07:34:44 +0530

    install

--- install-eks-k8.sh
+++ install-eks-k8.sh
@@ -6,3 +6,22 @@ eksctl version
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/
+
+lsblk
+sudo growpart /dev/nvme0n1 4
+sudo lvextend -l +50%FREE /dev/RootVG/rootVol
+sudo lvextend -l +50%FREE /dev/RootVG/varVol
+sudo xfs_growfs /
+sudo xfs_growfs /var
+
+#check whether root user or not
+R="\e[31m"
+N="\e[0m"
+
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
+echo -e "$R Logout and Login again $N"
