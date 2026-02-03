dba3f4900c6e43f93482d3ec2baf2c1d4f72f3d9
msaivenkatasiva <sivasl0597@gmail.com>
2026-02-03 08:09:30 +0530

    k8

--- install-eks-k8.sh
+++ install-eks-k8.sh
@@ -1,18 +1,30 @@
 #!bin/bash
+
+#installing eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
+#installing kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/
 
+#installing kubens
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+
+#mounting extra volume
 lsblk
 sudo growpart /dev/nvme0n1 4
 sudo lvextend -l +50%FREE /dev/RootVG/rootVol
 sudo lvextend -l +50%FREE /dev/RootVG/varVol
 sudo xfs_growfs /
 sudo xfs_growfs /var
+
+
+#installing docker
 
 #check whether root user or not
 R="\e[31m"
@@ -25,3 +37,8 @@ systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
 echo -e "$R Logout and Login again $N"
+
+#installing helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 get_helm.sh
+./get_helm.sh
